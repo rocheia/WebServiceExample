9cb8769a9acc770ec90e45285a6acf7785f21926
Ian Roche <ian.1.roche@gmail.com>
2022-12-12 11:01:29 -0500

    Updates

--- .ci/osutil.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional, List
-import subprocess
-
-
-def execute(cls, cmd: List[str], working_directory: Optional[str] = None):
-    if working_directory is None:
-        process = subprocess.call(cmd, shell=True, capture_output=True, check=True)
-    else:
-        process = subprocess.call(cmd, shell=True, capture_output=True, check=True, cwd=working_directory)
